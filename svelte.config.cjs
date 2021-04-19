92db63a01f8c208a09365f4bb573448bd12f91ac
James Bradbury <jamesbradbury93@gmail.com>
2021-04-19 12:48:23 +0100

    add remark plugins

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,11 +1,19 @@
 const node = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 const { mdsvex } = require('mdsvex');
+const slug = require('remark-slug');
+const externalLinks = require('remark-external-links');
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	extensions: [ '.svelte', '.svx' ],
-	preprocess: [ mdsvex() ],
+	preprocess: [ mdsvex({
+		remarkPlugins: [
+			slug,
+			[externalLinks, {target: "_blank"}]
+		]
+	})
+	],
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
