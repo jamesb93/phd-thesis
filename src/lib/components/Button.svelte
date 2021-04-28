b3410810a6da819a092fcb0731d7648846e39d8f
James Bradbury <jamesbradbury93@gmail.com>
2021-04-28 13:42:09 +0100

    buttno component for new reacoma demos

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,75 @@
+<script>
+    export let text = "button"
+    export let clickHandler = () => {};
+    export let selected = null;
+</script>
+
+<button 
+class:unselected={ selected === false && selected !== null } 
+on:click={clickHandler} 
+class="btn zoom">
+{text}
+</button>
+
+<style>
+    .btn {
+        position: relative;
+        display: inline-block;
+        width: fit-content;
+        height: 2em;
+        padding: 5px;
+        font-size: 15px;
+        overflow: hidden;
+        border-width: 0;
+        outline: none;
+        border-radius: 1px;
+        box-shadow: 0 1px 4px rgba(0, 0, 0, .6);    
+        color: #ecf0f1;
+        transition: background-color .25s;
+    }
+    
+    .btn:before {
+        content: "";
+        
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        
+        display: block;
+        width: 0;
+        padding-top: 0;
+        
+        border-radius: 100%;
+        
+        background-color: rgba(236, 240, 241, .3);
+        
+        -webkit-transform: translate(-50%, -50%);
+        -moz-transform: translate(-50%, -50%);
+        -ms-transform: translate(-50%, -50%);
+        -o-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+    }
+    
+    .btn:active:before {
+        width: 120%;
+        padding-top: 120%;
+        transition: width .2s ease-out, padding-top .2s ease-out;
+    }
+    
+    .zoom {
+        background-color: darkcyan;
+        font-size: 15px;
+        min-width: 2em;
+    }
+    .zoom:hover, .zoom:focus {
+        background-color: rgb(2, 196, 196);
+    }
+
+    .unselected {
+        background-color:grey;
+    }
+
+    .unselected:hover, .unselected:focus {
+        background-color:rgb(102, 101, 101);
+    }
+</style>
---
+++ src/lib/demos/reacoma/MaxwaveStart.svelte
@@ -0,0 +1,160 @@
+<script>
+    import { onMount } from 'svelte';
+    import { browser } from '$app/env';
+    import Button from '$lib/components/Button.svelte';
+    
+    export let id = "";
+    export let title = "";
+    
+    const originalAudio = '/reacoma/dotmaxwave/unprocessed.mp3'
+    const originalData = '/reacoma/dotmaxwave/unprocessed.dat'
+    const processedAudio = '/reacoma/dotmaxwave/processed.mp3'
+    const processedData = '/reacoma/dotmaxwave/processed.dat'
+    
+    let instance, Peaks;
+    let zoom, overview, controls, audio, controller;
+    let whichSource = 'original';
+    
+    function setSource(audio, buffer, src) {
+        if (whichSource !== src) {
+            whichSource = src;
+            const options = {
+                mediaUrl: audio,
+                dataUri: { arraybuffer: buffer },
+            };
+            instance.setSource(options, err => {
+                if (err) console.log(err)
+            });
+        }
+    }
+    
+    onMount(async() => {
+        if (browser) {
+            const options = {
+                containers: {
+                    zoomview: zoom,
+                    overview: overview
+                },
+                dataUri: { arraybuffer: originalData },
+                mediaElement: audio,
+                height: 60,
+                segmentStartMarkerColor: '#a0a0a0',
+                segmentEndMarkerColor: '#a0a0a0',
+                zoomWaveformColor: 'rgba(0, 30, 128, 0.61)',
+                overviewWaveformColor: 'rgba(0, 15, 100, 0.3)',
+                overviewHighlightColor: 'grey',
+                segmentColor: 'rgba(255, 161, 39, 1)',
+                playheadColor: 'rgba(0, 0, 0, 1)',
+                playheadTextColor: '#aaa',
+                showPlayheadTime: false,
+                pointMarkerColor: '#FF0000',
+                axisGridlineColor: '#ccc',
+                axisLabelColor: '#aaa',
+                randomizeSegmentColor: true,
+                segments: null
+            }
+            const module = await import("peaks.js");
+            Peaks = module.default;
+            
+            Peaks.init(options, (err, p) => {
+                if (err) {  
+                    console.log(err)
+                } else {
+                    instance = p
+                }
+            });
+        }
+    })
+    
+</script>
+
+<div class="audio-box" id={id}>
+    
+    <div>
+        <span id="title">{title}</span>
+        <p>
+            Alternate between the transient and residual components by clicking the respective button.
+        </p>
+    </div>
+    <div class="horizontal">
+        <div>
+            <Button selected={whichSource === 'original'} clickHandler={ () => setSource(originalAudio, originalData, 'original') } text="Unprocessed Source" />
+            <Button selected={whichSource === 'processed'} clickHandler={ () => setSource(processedAudio, processedData, 'processed') } text="HPSS Processed Source" />
+        </div>
+        <div bind:this={controls} class="audio-controls">
+            <Button clickHandler={ () => instance.zoom.zoomOut() } text="+" />
+            <Button clickHandler={ () => instance.zoom.zoomIn() } text="-" />
+        </div>
+    </div>
+    <div class="vis">
+        <div bind:this={overview} />
+        <div bind:this={zoom} />
+    </div>
+    <div class="peaks-controls">
+        <audio controls bind:this={audio}>
+            <source bind:this={controller} src={originalAudio} type="audio/mp3">
+            <track kind="captions">
+        </audio>
+    </div>
+</div>
+        
+<style>
+    .audio-box {
+        padding-top: 20px;
+        margin-top: 20px;
+        padding-bottom: 20px;
+        border-radius: 12px;
+        border: 3px solid rgba(128, 128, 128, 0.575);
+        transition: border .5s;
+        padding-left: 20px;
+        padding-right: 20px;
+
+    }
+    
+    .audio-box:hover {
+        border: 3px solid #2b5aa1a2;
+        transition: border .5s;
+        gap: 3px;
+    }
+    
+    .audio-controls {
+        display: flex;
+        flex-direction: row;
+        gap : 5px;
+    }
+    
+    #title {
+        font-style: italic;
+        font-size: 16px;
+        text-align: center;
+        display: inline-block;
+    }
+    
+    #caption{
+        display: inline-block;
+    }
+    
+    .horizontal {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 10%;
+        padding-bottom: 15px;
+    }
+    
+    .vis {
+        padding-bottom: 5px;
+        margin: 0 auto;
+    }
+    
+    .peaks-controls {
+        display: flex;
+        justify-content: center;
+        padding-top: 3px;
+    }
+    
+    #timecodes {
+        font-weight: bold;
+    }
+</style>
+        
