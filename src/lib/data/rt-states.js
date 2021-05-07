57b4c9a21f0ba2c43d7913522546a9c4c31637a9
James Bradbury <jamesbradbury93@gmail.com>
2021-05-07 18:45:54 +0100

    move data to better location

---
+++ src/lib/data/rt-states.js
@@ -0,0 +1,52 @@
+export const states = [
+    {
+        startTime: 0.0,
+        endTime: 114.0,
+        labelText: 'State 1'
+    },
+    {
+        startTime: 114.0,
+        endTime: 122.0,
+        labelText: 'State 2'
+    },
+    {
+        startTime: 122.0,
+        endTime: 179.0,
+        labelText: 'State 3'
+    },
+    {
+        startTime: 179.0,
+        endTime: 206.0,
+        labelText: 'State 4'
+    },
+    {
+        startTime: 206.0,
+        endTime: 259.0,
+        labelText: 'State 5'
+    },
+    {
+        startTime: 259.0,
+        endTime: 313.0,
+        labelText: 'State 6'
+    },
+    {
+        startTime: 313.0,
+        endTime: 416.0,
+        labelText: 'State 7'
+    },
+    {
+        startTime: 416.0,
+        endTime: 481.0,
+        labelText: 'State 8'
+    },
+    {
+        startTime: 481.0,
+        endTime: 572.0,
+        labelText: 'State 9'
+    },
+    {
+        startTime: 572.0,
+        endTime: 646.0,
+        labelText: 'State 10'
+    }
+]
