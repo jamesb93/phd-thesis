fc0be4fee0e1cab84fbd745a70bdb82df5fd4b45
James Bradbury <jamesbradbury93@gmail.com>
2021-05-11 01:09:10 +0100

    in progress play/pause button

---
+++ src/lib/components/PlayPause.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let w, h;
+
+    const WIDTH = w;
+    const HEIGHT = h;
+
+    export let state = true; // true = play, false = paused
+    export let clickHandler = () => {};
+</script>
+
+<svg width={`${WIDTH}px`} height={`${HEIGHT}px`} on:click={clickHandler}>
+    {#if state}
+        <line x1=0 x2={WIDTH} y1=0 y2={HEIGHT / 2} />
+        <line x1=0 x2={WIDTH} y1={HEIGHT} y2={HEIGHT / 2} />
+        <line x1=0 x2=0 y1=0 y2={HEIGHT} />
+    {/if}
+</svg>
+
+<style>
+    line {
+        stroke: black;
+        stroke-width: 2px;
+    }
+</style>
