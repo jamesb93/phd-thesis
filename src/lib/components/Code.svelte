660188ecc6073c1bb19e755ba09c9601ae2a65e1
James Bradbury <jamesbradbury93@gmail.com>
2021-05-11 18:34:15 +0100

    speculative code component (maybe not necessary)

---
+++ src/lib/components/Code.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let caption = ""
+    export let figure = "CODE"
+    export let id='code'
+</script>
+
+<div id={id}>
+    <div class='sub-container'>
+        <slot />
+    </div>
+    
+    <div class='caption'>
+        {figure} : {caption}
+    </div>
+</div>
+
+
+<style>
+    .sub-container {
+        padding-left: 10px;
+        padding-right: 10px;
+    }
+
+    .caption {
+        font-style: italic;
+        text-align: center;
+        width: 80%;
+        margin: 0 auto;
+    }
+</style>
