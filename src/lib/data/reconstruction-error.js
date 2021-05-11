407ffb211d71a0ec78184b0252dcd1a32e2f7966
James Bradbury <jamesbradbury93@gmail.com>
2021-05-11 18:33:23 +0100

    put reconstruction error albums in their own js file

---
+++ src/lib/data/reconstruction-error.js
@@ -0,0 +1,45 @@
+export const reAlbum = {
+    prefix : '/pieces/re/',
+    trackData : [
+        { name: '--isn', audio: '--isn.mp3', peaks: '--isn.dat' },
+        { name: 'dotmaxwave', audio: 'dotmaxwave.mp3', peaks: 'dotmaxwave.dat' },
+        { name: 'segmnoittet', audio: 'segmnoittet.mp3', peaks: 'segmnoittet.dat' },
+        { name: 'sys.ji_', audio: 'sys.ji_.mp3', peaks: 'sys.ji_.dat' },
+        { name: 'X86Desc.a', audio: 'X86Desc.a.mp3', peaks: 'X86Desc.a.dat' },
+    ]
+}
+
+export const ibuffer = {
+    prefix: '/re/db-initial/',
+    trackData : [
+        { name: 'AEC.pdf', audio: 'AEC-attenuated.mp3',  peaks: 'AEC.dat' },
+        { name: 'carl.png', audio: 'carl-attenuated.mp3',  peaks: 'carl.dat' },
+        { name: 'crash.rtf', audio: 'crash-attenuated.mp3',  peaks: 'crash.dat' },
+        { name: 'clickers.RPP', audio: 'clickers-attenuated.mp3',  peaks: 'clickers.dat' },
+        { name: 'er022ew.pdf', audio: 'er022ew-attenuated.mp3',  peaks: 'er022ew.dat' },
+        { name: 'picture.raw', audio: 'picture-attenuated.mp3',  peaks: 'picture.dat' },
+        { name: 'screenie.pdf', audio: 'screenie-attenuated.mp3',  peaks: 'screenie.dat' },
+        { name: 'Screenshot 2019-04-26 at 11.04.50.png', audio: 'Screenshot 2019-04-26 at 11.04.50-attenuated.mp3',  peaks: 'Screenshot 2019-04-26 at 11.04.50.dat' },
+        { name: 'Screenshot 2019-04-26 at 11.05.00.png', audio: 'Screenshot 2019-04-26 at 11.05.00-attenuated.mp3',  peaks: 'Screenshot 2019-04-26 at 11.05.00.dat' }
+    ]
+}
+
+export const generation = {
+    prefix: '/re/generation/',
+    trackData : [
+        { name: 'libLLVMAMDGPUDesc.a', audio: 'libLLVMAMDGPUDesc.a.mp3', peaks: 'libLLVMAMDGPUDesc.a.dat'},
+        { name: 'rainstick.aif.maxwave', audio: 'rainstick.aif.maxwave.mp3', peaks: 'rainstick.aif.maxwave.dat'},
+        { name: 'Library.storedata-shm', audio: 'Library.storedata-shm.mp3', peaks: 'Library.storedata-shm.dat'},
+        { name: '01-190328_1317-glued-08.wav.reapeaks', audio: '01-190328_1317-glued-08.wav.reapeaks.mp3', peaks: '01-190328_1317-glued-08.wav.reapeaks.dat' },
+        { name: '5253E668F50FA300CF9497D3952DBDE3837C6CB9', audio: '5253E668F50FA300CF9497D3952DBDE3837C6CB9.mp3', peaks: '5253E668F50FA300CF9497D3952DBDE3837C6CB9.dat' },
+        { name: 'ABAssistantChangelog.aclcddb-wal', audio: 'ABAssistantChangelog.aclcddb-wal.mp3', peaks: 'ABAssistantChangelog.aclcddb-wal.dat'},
+        { name: 'cache_54_3', audio: 'cache_54_3.mp3', peaks: 'cache_54_3.dat'},
+        { name: 'Default_4.0.ReaperThemeZip', audio: 'Default_4.0.ReaperThemeZip.mp3', peaks: 'Default_4.0.ReaperThemeZip.dat'},
+        { name: 'Default_5.0.ReaperThemeZip', audio: 'Default_5.0.ReaperThemeZip.mp3', peaks: 'Default_5.0.ReaperThemeZip.dat'},
+        { name: 'knowledgeC.db', audio: 'knowledgeC.db.mp3', peaks: 'knowledgeC.db.dat'},
+        { name: 'maxcrt.lib', audio: 'maxcrt.lib.mp3', peaks: 'maxcrt.lib.dat'},
+        { name: 'modules.log', audio: 'modules.log.mp3', peaks: 'modules.log.dat'},
+        { name: 'odr.sqlite', audio: 'odr.sqlite.mp3', peaks: 'odr.sqlite.dat'},
+        { name: 'QuotaManager', audio: 'QuotaManager.mp3', peaks: 'QuotaManager.dat'}
+    ]
+}
