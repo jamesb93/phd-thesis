0925d4198816af3167c154e7e13902549c48e4fc
James Bradbury <jamesbradbury93@gmail.com>
2021-05-12 13:18:07 +0100

    add slice data

--- src/lib/data/reconstruction-error.js
+++ src/lib/data/reconstruction-error.js
@@ -43,3 +43,33 @@ export const generation = {
         { name: 'QuotaManager', audio: 'QuotaManager.mp3', peaks: 'QuotaManager.dat'}
     ]
 }
+
+export const libllvmSlices = [
+    { startTime: 0.0, endTime: 2.3, labelText: 'Slice 1' },
+    { startTime: 2.3, endTime: 6.47, labelText: 'Slice 2' },
+    { startTime: 6.47, endTime: 7.255, labelText: 'Slice 3' },
+    { startTime: 7.255, endTime: 11.823, labelText: 'Slice 4' },
+    { startTime: 11.823, endTime: 28.886, labelText: 'Slice 5' },
+    { startTime: 28.886, endTime: 33.0, labelText: 'Slice 6' }
+]
+
+export const noveltySlices = [
+    { startTime: 0.0, endTime: 0.512, labelText: 'Slice 1' },
+    { startTime: 0.512, endTime: 0.673 , labelText: 'Slice 2' },
+    { startTime: 0.673, endTime: 1.393 , labelText: 'Slice 3' },
+    { startTime: 1.393, endTime: 1.532 , labelText: 'Slice 4' },
+    { startTime: 1.532, endTime: 1.811 , labelText: 'Slice 5' },
+    { startTime: 1.811, endTime: 2.298 , labelText: 'Slice 6' },
+    { startTime: 2.298, endTime: 6.478 , labelText: 'Slice 7' },
+    { startTime: 6.478, endTime: 7.383 , labelText: 'Slice 8' },
+    { startTime: 7.383, endTime: 7.523 , labelText: 'Slice 9' },
+    { startTime: 7.523, endTime: 10.565 , labelText: 'Slice 10' },
+    { startTime: 10.565, endTime: 11.726 , labelText: 'Slice 11' },
+    { startTime: 11.726, endTime: 24.803 , labelText: 'Slice 12' },
+    { startTime: 24.803, endTime: 26.832 , labelText: 'Slice 13' },
+    { startTime: 26.832, endTime: 29.054 , labelText: 'Slice 14' },
+    { startTime: 29.054, endTime: 29.187 , labelText: 'Slice 15' },
+    { startTime: 29.187, endTime: 30.116 , labelText: 'Slice 16' },
+    { startTime: 30.116, endTime: 31.881 , labelText: 'Slice 17' },
+    { startTime: 31.881, endTime: 32.743 , labelText: 'Slice 18' },
+]
