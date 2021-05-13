7444118c27758b514721d71069752456b365eee8
James Bradbury <jamesbradbury93@gmail.com>
2021-05-13 20:09:30 +0100

    remove captions they are annoying as hell

--- src/lib/components/VideoMedia2.svelte
+++ src/lib/components/VideoMedia2.svelte
@@ -9,7 +9,6 @@
 <Container id={id}>
     <video controls loop>
         <source src={url} type="video/mp4">
-        <track kind="captions" />
         <p>Your browser doesn't support HTML5 video. Here is a <a href={url}>link to the video</a> instead.</p> 
         </video>
     <div class="caption">
