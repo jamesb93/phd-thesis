38601606a2af91dc4e9cbaa64a2afd3bf61fa2e7
James Bradbury <jamesbradbury93@gmail.com>
2021-05-16 16:06:57 +0100

    script to create binary data for waveforms

---
+++ create_binary_data.sh
@@ -0,0 +1,5 @@
+fd -e dat -x rm {}
+cd static
+fd -e mp3 --exclude pieces -x audiowaveform -i {} -o {.}.dat -b 8 -z 100
+cd pieces
+fd -e mp3 -x audiowaveform -i {} -o {.}.dat --split-channels -b 8 -z 100 
