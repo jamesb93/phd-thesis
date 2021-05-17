509a588110993a3fdfe5406d5c967082140e7279
James Bradbury <jamesbradbury93@gmail.com>
2021-05-17 18:04:02 +0100

    data for interferences

---
+++ src/lib/data/interferences.js
@@ -0,0 +1,7 @@
+export const interAlbum = {
+    prefix : '/pieces/inter/',
+    trackData : [
+        { name: 'eee', audio: 'eee.mp3', peaks: 'eee.dat' },
+        { name: 'anchors', audio: 'anchors.mp3', peaks: 'anchors.dat' },
+    ]
+}
