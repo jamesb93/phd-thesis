32d6a05797e3faa78277f4e40f04549724076e0b
James Bradbury <jamesbradbury93@gmail.com>
2021-05-17 20:12:46 +0100

    add acknowledgements

--- src/lib/components/StaticNav.svelte
+++ src/lib/components/StaticNav.svelte
@@ -16,28 +16,29 @@
 <nav class="container">
 	<Button clickHandler={handleButton} text="toggle justification"/>
 	<ul>
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === "/"}'	href='.'>Home</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === "/"}	href='.'>Home</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === "/acknowledgements"} href='/acknowledgements'>Acknowledgements</a></li>
 		
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.intro}' href={m.intro}>1. Introduction</a></li>
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.preoc}' href={m.preoc}>2. Preoccupations</a></li>
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.ca}' href={m.ca}>3. Content-Awareness</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.intro} href={m.intro}>1. Introduction</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.preoc} href={m.preoc}>2. Preoccupations</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.ca} href={m.ca}>3. Content-Awareness</a></li>
 		
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.proj}' href={m.proj}>4. Projects</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.ss}' href={m.ss}>4.1 Stitch/Strata</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.as}' href={m.as}>4.2 Annealing Strategies</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.rt}' href={m.rt}>4.3 Refracted Touch</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.re}' href={m.re}>4.4 Reconstruction Error</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.em}' href={m.em}>4.5 Interferences</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.proj} href={m.proj}>4. Projects</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.ss} href={m.ss}>4.1 Stitch/Strata</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.as} href={m.as}>4.2 Annealing Strategies</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.rt} href={m.rt}>4.3 Refracted Touch</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.re} href={m.re}>4.4 Reconstruction Error</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.em} href={m.em}>4.5 Interferences</a></li>
 
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.ti}' href="/tech">5. Technical Implementation</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.mosh}' href={m.mosh}>5.1 mosh</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.reacoma}' href={m.reacoma}>5.2 ReaCoMa</a></li>
-		<li><a  on:click={handleClick} class="link subsection" class:selected='{$page.path === m.ftis}' href={m.ftis}>5.3 FTIS</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.ti} href="/tech">5. Technical Implementation</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.mosh} href={m.mosh}>5.1 mosh</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.reacoma} href={m.reacoma}>5.2 ReaCoMa</a></li>
+		<li><a  on:click={handleClick} class="link subsection" class:selected={$page.path === m.ftis} href={m.ftis}>5.3 FTIS</a></li>
 
 
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.conc}' href={m.conc}>6. Conclusion</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.conc} href={m.conc}>6. Conclusion</a></li>
 		
-		<li><a  on:click={handleClick} class="link section" class:selected='{$page.path === m.ref}' href={m.ref}>References and Links</a></li>
+		<li><a  on:click={handleClick} class="link section" class:selected={$page.path === m.ref} href={m.ref}>References and Links</a></li>
 
 		<br>
 		<li><a target="_blank" on:click={handleClick} class="faint" href="https://github.com/jamesb93/phd-sapper">source code</a></li>
