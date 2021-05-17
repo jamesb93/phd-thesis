64c6aa8a73d66a78b3e533f12b3c15d0bfc5fcf9
James Bradbury <jamesbradbury93@gmail.com>
2021-05-17 20:12:49 +0100

    add data for interferences

--- src/lib/data/interferences.js
+++ src/lib/data/interferences.js
@@ -5,3 +5,34 @@ export const interAlbum = {
         { name: 'anchors', audio: 'anchors.mp3', peaks: 'anchors.dat' },
     ]
 }
+
+export const kindleModes = [
+    {
+        startTime : 0.0,
+        endTime: 1.83,
+        labelText: 'Initial static state.'
+    },
+    {
+        startTime: 1.83,
+        endTime: 119.0,
+        labelText: 'Activity induced through user interaction.'
+    },
+    {
+        startTime: 119.0,
+        endTime: 127.0,
+        labelText: 'Static state returns after activity ends.'
+    }
+]
+
+export const xiaomiModes = [
+    {
+        startTime : 0.0,
+        endTime: 40.0,
+        labelText: 'Static state.'
+    },
+    {
+        startTime: 40.0,
+        endTime: 102.0,
+        labelText: 'Active state.'
+    }
+]
