567569143bb45cd2cdb9042b2fbdd2f657d5201b
James Bradbury <jamesbradbury93@gmail.com>
2021-05-21 00:07:46 +0100

    report progres sbut nothing else

--- video-conversion.py
+++ video-conversion.py
@@ -7,8 +7,11 @@ from pathlib import Path
 videos = [x for x in Path("static").rglob('*.mov')]
 
 for i, x in enumerate(videos):
-    print(i)
-    first_pass = f"ffmpeg -i {str(x)} -hide_banner -loglevel error -c:v libvpx-vp9 -b:v 0 -crf 23 -pass 1 -an -f null /dev/null"
-    second_pass = f"ffmpeg -i {str(x)} -hide_banner -loglevel error -c:v libvpx-vp9 -b:v 0 -crf 23 -ac 2 -pass 2 -c:a libopus {str(x.with_suffix('.webm'))} -y"
+    print('-'*10)
+    print(str(x))
+    print(f'{i} of {len(videos)}')
+    print('-'*10)
+    first_pass = f"ffmpeg -i {str(x)} -hide_banner -v quiet -stats -c:v libvpx-vp9 -b:v 0 -crf 23 -pass 1 -an -f null /dev/null"
+    second_pass = f"ffmpeg -i {str(x)} -hide_banner -v quiet -stats -c:v libvpx-vp9 -b:v 0 -crf 23 -ac 2 -pass 2 -c:a libopus {str(x.with_suffix('.webm'))} -y"
     subprocess.call(first_pass.split(' '))
     subprocess.call(second_pass.split(' '))
