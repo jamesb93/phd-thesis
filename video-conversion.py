874772a88f8bf72171255176e222f23f6886efa8
James Bradbury <jamesbradbury93@gmail.com>
2021-05-20 23:52:04 +0100

    work on video converter

--- video-conversion.py
+++ video-conversion.py
@@ -1,12 +1,14 @@
+# https://trac.ffmpeg.org/wiki/Encode/VP9
+# Uses a two pass encoder to get high quality stereo WEBM's for compatability with most browsers.
+
 import subprocess
 from pathlib import Path
 
 videos = [x for x in Path("static").rglob('*.mov')]
 
-for x in videos:
-    print(str(x))
+for i, x in enumerate(videos):
+    print(i)
     first_pass = f"ffmpeg -i {str(x)} -c:v libvpx-vp9 -b:v 0 -crf 23 -pass 1 -an -f null /dev/null"
     second_pass = f"ffmpeg -i {str(x)} -c:v libvpx-vp9 -b:v 0 -crf 23 -ac 2 -pass 2 -c:a libopus {str(x.with_suffix('.webm'))} -y"
     subprocess.call(first_pass.split(' '))
     subprocess.call(second_pass.split(' '))
-    quit()
