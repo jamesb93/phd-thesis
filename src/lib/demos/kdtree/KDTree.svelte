d5492cbf1ed17dafcf3f7688c45bf34fc24f8c50
James Bradbury <jamesbradbury93@gmail.com>
2021-05-26 18:12:22 +0100

    nuance the tree

--- src/lib/demos/kdtree/KDTree.svelte
+++ src/lib/demos/kdtree/KDTree.svelte
@@ -1,1 +1,109 @@
-<!-- Should show 100 points and alawys select the nearest 10 to the mouse -->
+<!-- 
+    Refer to:
+    https://ptsjs.org/guide/op-0400s
+ -->
+
+<script>
+    import { onMount } from 'svelte';
+    import { CanvasSpace, Create } from 'pts';
+    import Container from '$lib/components/Container.svelte';
+
+    export let title = 'k-d tree sample searching'
+    export let caption = 'DEMO'
+    export let id = ''
+
+    let canvas;
+    let ready = false;
+    let pts;
+    let mouse
+    let rect;
+
+    function getMousePos(canvas, evt) {
+        // We need to do this manually otherwise when shifting the window the resize is not accounted for.
+        rect = canvas.getBoundingClientRect();
+        return [
+            (evt.clientX - rect.left) / (rect.right - rect.left) * canvas.width,
+            (evt.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height
+        ]
+    }
+
+    onMount(async() => {
+        let space = new CanvasSpace('#sketch').setup({ bgcolor: '#fff' });
+        let form = space.getForm();
+        rect = canvas.getBoundingClientRect();
+        space.add({
+            start: (bounds) => {
+                pts = Create.distributeRandom( space.innerBound, 300 );
+            },
+            animate: (time, ftime, space) => {
+                pts.sort( (a,b) => a.$subtract(mouse).magnitudeSq() - b.$subtract(mouse).magnitudeSq() );
+                for (let i=1; i < 10; i++) {
+                    form.fillOnly("#123").points( pts.slice(0, 10), 3, "circle" );
+                }
+                for (let i=0; i < 10; i++) {
+                    form.strokeOnly('#0d47a1', 2).line( [ pts[i], mouse ] );
+                }
+                form.fill("#f03").point( pts[0], 6, "circle" );
+                form.fillOnly("#123").points( pts.slice(10), 2, "circle" );
+            },
+            action: (t,x,y,e) => {
+                mouse = getMousePos(canvas, e)
+            }
+        });
+        space.play().bindMouse();
+        ready = true;
+    })
+</script>
+
+<Container zoom={false} id={id}>
+    <div id='top-text'>
+        <div id='info'>
+            <span id='title'>{title}</span>
+            <span id='caption'>{caption}</span>
+        </div>
+        <div id='explanation'>
+            <p>    
+                Each point in this space represents an audio sample, reduced into a two-dimensional representation through analysis and dimension reduction.
+            </p>
+            <p>
+                As you move the mouse, the nearest sample to the pointer is highlighted in red. The 10 samples closest to this have lines drawn to them and are emboldened.
+            </p>
+            <p>
+                This method of navigating through the sample space depicts my <i>k-d approach</i> to finding sounds which are similar to a specifically selected sample within a corpus. The selected sampl is represented by the highlighted red point, and the 10 most similar sounds are derived by branching out into nearby space.
+            </p>
+        </div>
+    </div>
+    <div id='drawing-zone'>        
+        {#if ready === false}
+        <span>Loading UMAP Data...</span>
+        {/if}
+        <canvas id='sketch' bind:this={ canvas } />
+    </div>
+</Container>
+
+<style>
+    #sketch {
+        width: 100%;
+        height: 600px;
+    }
+
+    #top-text {
+        display: flex;
+        flex-direction: column;
+    }
+
+    #info {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+
+    #title {
+        font-weight: bold;
+    }
+
+    #caption {
+        font-style: italic;
+    }
+</style>
+
