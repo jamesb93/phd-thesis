56a2615c91b78e5893a5fff113192c1e78fa8dc9
James Bradbury <jamesbradbury93@gmail.com>
2021-05-26 13:20:52 +0100

    super zoom level

--- create_binary_data.sh
+++ create_binary_data.sh
@@ -1,5 +1,5 @@
 fd -e dat -x rm {}
 cd static
-fd -e mp3 --exclude pieces -x audiowaveform -i {} -o {.}.dat -b 8 -z 100
+fd -e mp3 --exclude pieces -x audiowaveform -i {} -o {.}.dat -b 8 -z 32
 cd pieces
 fd -e mp3 -x audiowaveform -i {} -o {.}.dat --split-channels -b 8 -z 100 
