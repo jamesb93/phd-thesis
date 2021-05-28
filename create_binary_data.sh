0fafce9dc6be967bbbdc35aaee6a993bb9b8c6cd
James Bradbury <jamesbradbury93@gmail.com>
2021-05-28 11:50:13 +0100

    reduce resolution for pieces a bit

--- create_binary_data.sh
+++ create_binary_data.sh
@@ -2,4 +2,4 @@ fd -e dat -x rm {}
 cd static
 fd -e mp3 --exclude pieces -x audiowaveform -i {} -o {.}.dat -b 8 -z 32
 cd pieces
-fd -e mp3 -x audiowaveform -i {} -o {.}.dat --split-channels -b 8 -z 100 
+fd -e mp3 -x audiowaveform -i {} -o {.}.dat --split-channels -b 8 -z 128 
