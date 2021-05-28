59dbe7beb13f196595b1548ba04c947d468d00a1
James Bradbury <jamesbradbury93@gmail.com>
2021-05-28 20:01:36 +0100

    we may move the directory to a store later

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,8 +1,7 @@
-import { writable, get } from 'svelte/store';
+import { writable, readable } from 'svelte/store';
 
+// Durations for all audio files in the thesis
 export const durations = writable({});
-
-
 async function getDurations() {
     try {
         const response = await fetch('/durations.json')
@@ -11,8 +10,29 @@ async function getDurations() {
         console.log(e)
     }
 }
-
 getDurations()
+
+// export const directory = readable({
+//     intro : '/introduction',
+//     preoc : '/preoccupations',
+//     ca : '/content-awareness',
+//     proj : '/projects',
+//     ss : '/projects/stitch-strata',
+//     as : '/projects/annealing-strategies',
+//     rt : '/projects/refracted-touch',
+//     re : '/projects/reconstruction-error',
+//     em : '/projects/electromagnetic',
+//     emname: 'Interferences',
+//     conc : '/conclusion',
+//     ti : '/tech',
+//     mosh : '/tech/mosh',
+//     reacoma : '/tech/reacoma',
+//     ftis : '/tech/ftis',
+//     ref : '/references',
+//     biom : 'https://www.jamesbradbury.xyz/projects/biomimicry',
+//     anchors: 'anchors',
+//     eee: 'eee'
+// }) 
 
 
 
