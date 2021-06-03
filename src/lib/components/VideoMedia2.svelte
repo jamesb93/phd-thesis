58b851e06028a4044344c8af100bd4d6b966bd4a
James Bradbury <jamesbradbury93@gmail.com>
2021-06-03 13:54:50 +0100

    support an mp4 fallback to improve browser coverage

--- src/lib/components/VideoMedia2.svelte
+++ src/lib/components/VideoMedia2.svelte
@@ -8,9 +8,11 @@
 
 <Container id={id}>
     <video controls loop>
-        <source src={url} type="video/mp4">
+        <source src={url+'.webm'} type="video/webm">
+        <source src={url+'.mp4'} type="video/mp4">
+
         <p>Your browser doesn't support HTML5 video. Here is a <a href={url}>link to the video</a> instead.</p> 
-        </video>
+    </video>
     <div class="caption">
         {figure}: {caption}
     </div>
