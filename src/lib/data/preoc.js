51cd91fa7da17bc6cf957bd23db5086cdd312e53
James Bradbury <jamesbradbury93@gmail.com>
2021-06-04 12:37:11 +0100

    example of hierarchical form

---
+++ src/lib/data/preoc.js
@@ -0,0 +1,13 @@
+export const formSegs = [
+    {startTime: 6.177, endTime: 6.691, labelText: 'Short interjecting gesture (1)'},
+    {startTime: 7.829, endTime: 8.389, labelText: 'Short interjecting gesture (2)'},
+    {startTime: 14.731, endTime: 16.353, labelText: 'Longer interjecting gesture (1)'},
+    {startTime: 23.238, endTime: 25.161, labelText: 'Longer interjecting gesture (2)'},
+    {startTime: 16.244, endTime: 22.32, labelText: 'Gestures begin to amalgamate into a sequence'},
+    {startTime: 25.444, endTime: 35.0, labelText: 'Multiple gestures culminate in a fully blended sequence'}
+]
+
+export const formPoints = [
+    { time: 11.814, editable: false, labelText: 'Sequence emerges and persists (1)'},
+    { time: 16.244, editable: false, labelText: 'Sequence emerges and persists (2)'},
+]
