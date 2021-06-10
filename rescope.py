862e97ea60f306b66dca255a6cf3e6573813db18
James Bradbury <jamesbradbury93@gmail.com>
2021-06-10 13:40:33 +0100

    rescope script

--- rescope.py
+++ rescope.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 lookup = {
@@ -14,8 +15,45 @@ lookup = {
     'reacoma' : '5.3.'
 }
 
+widgets = ['AUDIO', 'VIDEO', 'CODE', 'DEMO', 'IMAGE']
+
+def replace(text: str, prefix: str) -> str:
+    temporary_text: list = []
+    for line in text:
+        for widget in widgets:
+            for i in range(25):
+                
+                reg = f'[{widget} {i}]'
+                search = re.search(reg, line)
+                if search:
+                    scoped_figure = f'[{widget} {prefix}{i}]'
+                    line = line.replace(reg, scoped_figure)
+
+                reg = f'"{widget} {i}"'
+                search = re.search(reg, line)
+                if search:
+                    scoped_figure = f"'{widget} {prefix}{i}'"
+                    line = line.replace(reg, scoped_figure)
+
+                reg = f"'{widget} {i}'"
+                search = re.search(reg, line)
+                if search:
+                    scoped_figure = f"'{widget} {prefix}{i}'"
+                    line = line.replace(reg, scoped_figure)
+
+        temporary_text.append(line)
+    
+    return ' '.join(temporary_text)
+
 routes = [x for x in Path("src/routes").rglob('*.svx')]
 sections = [x for x in routes if x.stem in lookup.keys()]
 
-for x in sections:
-    print(x.stem)
+for section in sections:
+    prefix = lookup[section.stem]
+    updated_text = None
+    with open(section, 'r') as f:
+        text = f.readlines()
+        updated_text = replace(text, prefix)
+    with open(section, 'w') as out_file:
+        out_file.write(updated_text)
+            
