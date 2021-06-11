40b8f385ae11b3c871e12c76e29c6792586f951d
James Bradbury <jamesbradbury93@gmail.com>
2021-06-11 15:42:05 +0100

    insert the heading prefix into the structure

--- get_structure.py
+++ get_structure.py
@@ -30,11 +30,41 @@ route_lookup = {
     'tech' : '/tech'
 }
 
+hierarchy = {
+    'howto' : 'ii',
+    'submission-materials': 'iv',
+    'list-of-interactive-elements' : 'v',
+    'introduction' : '1',
+    'preoccupations' : '2',
+    'content-awareness' : '3',
+    'stitch-strata' : '4.1',
+    'annealing-strategies' : '4.2',
+    'refracted-touch' : '4.3',
+    'reconstruction-error' : '4.4',
+    'interferences' : '4.5',
+    'mosh' : '5.1',
+    'ftis' : '5.2',
+    'reacoma' : '5.3',
+    'conclusion' : '',
+    'copyright' : '',
+    'references' : ''
+}
+
 structure = {}
 
+
 for page in svx:
+    c = {
+        1 : 0,
+        2 : 0,
+        3 : 0,
+        4 : 0,
+        5 : 0
+    }
     route = route_lookup[page.parent.name]
     path = f'{route}/{page.stem}'
+    prefix = hierarchy[page.stem]
+    print(page.stem, prefix)
     structure[path] = []
     with open(page) as text:
         lines = text.readlines();
@@ -43,9 +73,27 @@ for page in svx:
                 heading = get_heading(line)
                 url = get_url(line)
                 indent = line.count('#')
+                print(heading, indent)
+
+                calculated_heading = ''
+                
+                c[indent] += 1
+                if indent == 1:
+                    calculated_heading = f'{prefix} {heading}'
+                elif indent == 2:
+                    calculated_heading = f'{prefix}.{c[2]} {heading}'
+                elif indent == 3:
+                    calculated_heading = f'{prefix}.{c[2]}.{c[3]} {heading}'
+                elif indent == 4:
+                    calculated_heading = f'{prefix}.{c[2]}.{c[3]}.{c[4]} {heading}'
+                elif indent == 5:                    
+                    calculated_heading = f'{prefix}.{c[2]}.{c[3]}.{c[4]}.{c[5]} {heading}'
+                for i in range(indent+1, 5):
+                    c[i] = 0
+
                 structure[path].append(
                     {
-                        'heading' : heading,
+                        'heading' : calculated_heading,
                         'indent' : indent,
                         'url' : url
                     }
