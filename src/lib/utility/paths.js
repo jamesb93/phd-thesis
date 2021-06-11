ce8750e7a8fd3f96024932a4773883d4861c2475
James Bradbury <jamesbradbury93@gmail.com>
2021-06-12 00:55:24 +0100

    properly form links to escape urls

--- src/lib/utility/paths.js
+++ src/lib/utility/paths.js
@@ -1,5 +1,5 @@
 export const cloudPrefix = 'https://jbphd-pub.s3.us-west-000.backblazeb2.com'
 
 export const noext = (str) => {
-    return str.slice(0, -4)
+    return str.slice(0, -4).replace('+', '%2B')
 }
